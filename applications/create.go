783177756fc625456ed5a64e1a7cbefa61faa562
andrewbackes <rbackes@ucla.edu>
2018-08-06 10:10:56 -0700

    Add creation options

--- applications/create.go
+++ applications/create.go
@@ -4,12 +4,26 @@ import (
 	"github.com/armory/plank/tasks"
 )
 
+// CreationOption used when making a new application.
+type CreationOption int
+
+const (
+	// WaitForCompletion of the Create operation before returning.
+	WaitForCompletion CreationOption = iota
+)
+
 // Create an application using the DefaultService.
-func Create(a Application) (tasks.ID, error) {
+func Create(a Application, o ...CreationOption) (tasks.ID, error) {
 	return DefaultService.Create(a)
 }
 
 // Create an application.
-func (s *Service) Create(a Application) (tasks.ID, error) {
+func (s *Service) Create(a Application, opts ...CreationOption) (tasks.ID, error) {
+	for _, o := range opts {
+		switch o {
+		case WaitForCompletion:
+			// block until complete
+		}
+	}
 	return "", nil
 }
