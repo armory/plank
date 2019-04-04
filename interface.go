e9a989dc1c01c8a186c79861c4c0ca14d6c3ca2e
Paul <paul@armory.io>
2019-04-04 10:21:42 -0700

    Create an interface and a mock Client
    Using gomock to allow other code to mock out the Plank calls.

---
+++ interface.go
@@ -0,0 +1,25 @@
+package plank
+
+type PlankClient interface {
+	// Applications
+	GetApplication(string) (*Application, error)
+	GetApplications() (*[]Application, error)
+
+	// Permissions
+	IsAdmin(string) (bool, error)
+	HasAppWriteAccess(string, string) (bool, error)
+	GetUser(string) (*User, error)
+
+	// Pipelines
+	GetPipeline(string, string) (*Pipeline, error)
+	GetPipelines(string) ([]Pipeline, error)
+	UpsertPipeline(Pipeline) error
+	DeletePipeline(Pipeline) error
+	DeletePipelineByName(string, string) error
+	Execute(string, string) (*PipelineRef, error)
+
+	// Tasks
+	GetTask(string) (*ExecutionStatusResponse, error)
+	PollTaskStatus(string) (*ExecutionStatusResponse, error)
+	CreateTask(string, string, interface{}) (*TaskRefResponse, error)
+}
