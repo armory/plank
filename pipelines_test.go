b3d990a6b881aca1b6812d6e5ab90c8b772bac7b
Todd Underwood <Todd.Underwood@armory.io>
2021-05-13 14:30:35 -0700

    feat(enable_gate_endpoints): add additional testing for pipelines

--- pipelines_test.go
+++ pipelines_test.go
@@ -58,6 +58,72 @@ func TestGetPipelinesWithGate(t *testing.T) {
 	assert.Equal(t, len(val), 0) // Should get 0 pipelines back.
 }
 
+func TestUpsertPipelinesNoIdWithGate(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		assert.Equal(t, req.URL.String(), "http://localhost:8084/plank/pipelines")
+		assert.Equal(t, req.Method, "POST")
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("[]")),
+			Header:     make(http.Header),
+		}
+	})
+
+	c := New(WithClient(client))
+	c.UseGateEndpoints()
+	err := c.UpsertPipeline(Pipeline{},"", "")
+	assert.Nil(t, err)
+}
+
+func TestUpsertPipelinesWithGate(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		assert.Equal(t, req.URL.String(), "http://localhost:8084/plank/pipelines/Test")
+		assert.Equal(t, req.Method, "PUT")
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("[]")),
+			Header:     make(http.Header),
+		}
+	})
+
+	c := New(WithClient(client))
+	c.UseGateEndpoints()
+	err := c.UpsertPipeline(Pipeline{},"Test", "")
+	assert.Nil(t, err)
+}
+
+func TestUpsertPipelines(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		assert.Equal(t, req.URL.String(), "http://localhost:8080/pipelines/Test")
+		assert.Equal(t, req.Method, "PUT")
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("[]")),
+			Header:     make(http.Header),
+		}
+	})
+
+	c := New(WithClient(client))
+	err := c.UpsertPipeline(Pipeline{},"Test", "")
+	assert.Nil(t, err)
+}
+
+func TestUpsertPipelinesNoId(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		assert.Equal(t, req.URL.String(), "http://localhost:8080/pipelines")
+		assert.Equal(t, req.Method, "POST")
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("[]")),
+			Header:     make(http.Header),
+		}
+	})
+
+	c := New(WithClient(client))
+	err := c.UpsertPipeline(Pipeline{},"", "")
+	assert.Nil(t, err)
+}
+
 func TestPipeline_ValidateRefIds(t *testing.T) {
 	tests := map[string]struct {
 		stage           string
